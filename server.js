d60c4521f27abebb9ac838cac8e7148b12b4e8d6
hemanth kumar <hemanthkumar.m16@iiits.in>
2019-12-05 09:55:04 +0530

    express

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
 const express = require("express");
+const app = express();
+
+app.get("/",(req,res) => {
+	res.send("Server");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,() => console.log(`Server listening on ${PORT}`));
